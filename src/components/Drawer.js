05095c7983959933b96ffc2e96f20c9c627b91b3
Bryan Xol <xolbryan@gmail.com>
2022-03-16 18:27:30 -0600

    +Se realizo cambio

--- src/components/Drawer.js
+++ src/components/Drawer.js
@@ -124,7 +124,7 @@ export default function MiniDrawer() {
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" noWrap component="div">
-            Cinepolis
+            App Cinepolis
           </Typography>
         </Toolbar>
       </AppBar>
